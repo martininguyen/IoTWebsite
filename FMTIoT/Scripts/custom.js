e9622c137622d1c7132329973789884e827afc96
Johnson Gong <jgong1993@gmail.com>
2017-01-19 15:04:22 -0800

    open chart via button

--- FMTIoT/Scripts/custom.js
+++ FMTIoT/Scripts/custom.js
@@ -1,30 +1,7 @@
-﻿function displayLine() {
-    
-    var charts = document.getElementsByClassName("chartcontainer"); 
-    for(i = 0; i < charts.length; i++){
-        charts[i].style.visibility = "hidden";
-    }
-    var size = document.getElementsByClassName("size");
-    for (i = 0; i < charts.length; i++) {
-        charts[i].style.width = "0";
-        charts[i].style.height = "0";
-    }
-    document.getElementById("line").style.visibility = "visible";
-    document.getElementById("line_chart").style.width = "960";
-    document.getElementById("line_chart").style.height = "500";
-}
-
-function displayBi() {
-    var charts = document.getElementsByClassName("chartcontainer");
-    for(i = 0; i < charts.length; i++){
-        charts[i].style.visibility = "hidden";
-    }
-    var size = document.getElementsByClassName("size");
-    for (i = 0; i < charts.length; i++) {
-        charts[i].style.width = "0";
-        charts[i].style.height = "0";
-    }
-    document.getElementById("bi").style.visibility = "visible";
-    document.getElementById("bivariate").style.width = "960";
-    document.getElementById("bivariate").style.height = "500";
+﻿function displayChart(type) {
+    var e = document.getElementById(type);
+    if (e.style.display == 'block')
+        e.style.display = 'none';
+    else
+        e.style.display = 'block';
 }
