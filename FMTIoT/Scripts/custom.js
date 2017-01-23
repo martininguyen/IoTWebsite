085c9aab8835f7cef047fb631aafa4d661fc716f
Martin Nguyen <martinnguyen0618@gmail.com>
2017-01-23 08:30:34 -0800

    added animate and logo

--- FMTIoT/Scripts/custom.js
+++ FMTIoT/Scripts/custom.js
@@ -1,7 +1,18 @@
-﻿function displayChart(type) {
+﻿/*function displayChart(type) {
     var e = document.getElementById(type);
     if (e.style.display == 'block')
         e.style.display = 'none';
     else
         e.style.display = 'block';
-}
+}*/
+
+
+$("#lineButton").click(function () {
+    $(".chartcontainer").hide("slow");
+    $("#line").show("fast");
+});
+$("#biButton").click(function () {
+    $(".chartcontainer").hide("slow");
+    $("#bi").show("fast");
+});
+
